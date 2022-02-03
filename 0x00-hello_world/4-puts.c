2d9a64588fc57711c41cfc99a48e35b18551e967
David <4292@holbertonschool.com>
2022-02-03 17:46:46 +0000

    fixing betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	char str1[] = "\"Programming is like building a multilingual puzzle";
+char str1[] = "\"Programming is like building a multilingual puzzle";
 	
-	puts(str1);
+puts(str1);
 	
-	return (0);
+return (0);
 }
