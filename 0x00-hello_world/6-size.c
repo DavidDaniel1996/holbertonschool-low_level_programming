c49f2636ae442b1ba6cb6a691afce7733994f914
David <4292@holbertonschool.com>
2022-02-03 18:58:25 +0000

    betty fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,12 @@
 
 int main(void)
 {
-	char c;
-	int i;
-	long int n;
-	long long int l;
-	float f;
 
-	printf("Size of a char: %zu.\n", sizeof (char));
-	printf("Size of an int: %zu.\n", sizeof (int));
-	printf("Size of a long int: %zu.\n", sizeof (long int));
-	printf("Size of a long long int: %zu.\n", sizeof (long long int));
-	printf("Size of a float: %zu.\n", sizeof (float));
+	printf("Size of a char: %zu.\n", sizeof(char));
+	printf("Size of an int: %zu.\n", sizeof(int));
+	printf("Size of a long int: %zu.\n", sizeof(long int));
+	printf("Size of a long long int: %zu.\n", sizeof(long long int));
+	printf("Size of a float: %zu.\n", sizeof(float));
 
 	return (0);
 }
