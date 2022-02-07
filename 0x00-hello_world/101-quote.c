454180796eb30d7831a0c9a72f49c252405e0d6c
David <4292@holbertonschool.com>
2022-02-07 21:17:39 +0000

    return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (-1);
 }
