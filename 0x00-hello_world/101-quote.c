808f58e2b39d2d7d33d6f5fef60977f255d435d2
David <4292@holbertonschool.com>
2022-02-07 23:46:20 +0000

    work?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,10 @@
  * nbyte - Descriptor
  * Return: Always 0 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
+	int write(int filedes, const char *buf, unsigned int nbyte);
+
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (-1);
