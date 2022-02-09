44cd8d38dd7973c17e76f8f1fd9910ae96cac5f9
David <4292@holbertonschool.com>
2022-02-09 15:47:25 +0000

    jack

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,29 @@
 #include "main.h"
 
+/**
+ * jack_bauer - prints all minutes and hours in a day
+ *
+ * Return: void
+ */
 void jack_bauer(void)
 {
+
+	int h = 0;
+	int m = 0;
+
+	while (h <= 23)
+	{
+		while (m <= 59)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+		m = 0;
+	}
+}
