a6faf5f5776d0a5313511a14c146f8c1d44cc44d
David <4292@holbertonschool.com>
2022-02-09 20:35:05 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two variables
+ * @a: character for variable 1
+ * @b: character for variable 2
+ *
+ * Return: returns value of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,21 +14,21 @@ void times_table(void)
 
 	for (a = 0 ; a <= 9 ; a++)
 	{
-		_putchar(48);
+		_putchar('0');
 		for (b = 1 ; b <= 9 ; b++)
 		{
 			c = a * b;
-			_putchar(44);
-			_putchar(32);
+			_putchar(',');
+			_putchar(' ');
 			if (c <= 9)
 			{
-				_putchar(32);
-				_putchar(c + 48);
+				_putchar(' ');
+				_putchar(c + '0');
 			}
 			else
 			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
