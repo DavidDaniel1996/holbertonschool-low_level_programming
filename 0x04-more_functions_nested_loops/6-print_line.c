5802b28c375bd2dd68648fdeb8007d66f14410c6
David <4292@holbertonschool.com>
2022-02-10 18:17:58 +0000

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - prints lines n amount of times
+* @n: variable representing amount of times line is printed
+*
+* Return: Void
+*/
+void print_line(int n)
+{
+	int x = 0;
+
+	while (x <= n)
+	{
+		_putchar('_');
+		x++;
+	}
+	_putchar ('\n');
+}
