1aa200261f25d9495c9b387ca85a1f7ac2b803e1
David <4292@holbertonschool.com>
2022-02-10 14:51:06 +0000

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiplies two variables
+* @a: character for variable 1
+* @b: character for varaible 2
+*
+* Return: returns result of multiplication
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
