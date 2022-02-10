d0c270d304bcada10891ab80302ca184b9343a16
David <4292@holbertonschool.com>
2022-02-10 17:46:36 +0000

    1 through 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,13 @@ void more_numbers(void)
 	{
 		int n;
 
-		for (n = '0'; n <= '9'; n++)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar(n);
+			if (n >= 10)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
 		}
 		_putchar ('\n');
 	}
