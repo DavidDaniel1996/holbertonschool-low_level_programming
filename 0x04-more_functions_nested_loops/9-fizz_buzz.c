43a3fe44a3ed85c2e8fc57667b8666c10346c721
David <4292@holbertonschool.com>
2022-02-11 01:52:33 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	for (n = 1; n < 100; n++)
 	{
 		if (n % 3 == 0 && (n % 5) == 0)
 		{
@@ -28,6 +28,6 @@ int main(void)
 			printf("%d ", n);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
