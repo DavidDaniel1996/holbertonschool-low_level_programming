e505c244fd57d3ad9c948e8ebf468671b07ec311
David <4292@holbertonschool.com>
2022-02-11 21:23:22 +0000

    leap days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -28,10 +28,11 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
-		else if (year % 4 != 0)
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+	}
+
+	else if (year % 4 != 0)
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 	}
 }
