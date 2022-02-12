8e0cbadc81110306463dd2451ddc1fbf2b11fbd5
David <4292@holbertonschool.com>
2022-02-12 17:42:56 +0000

    testing my frankenstien's code

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -23,9 +23,18 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
-		else if ((year % 100 == 0) && (year % 400 != 0))
+		else if (((year % 100 == 0) && (year % 400 != 0)) && (month == 2 && day == 60))
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else if (((year % 100 == 0) && (year % 400 != 0)) && !(month == 2 && day == 60))
+		{
+			if (month > 2 && day >= 60)
+			{
+				day++;
+			}
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 		else
 		{
@@ -39,22 +48,10 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else if (year % 4 != 0)
 	{
-		if ((year % 4 == 0) || (year % 400 == 0 && year % 100 == 0))
-		{
-			if (month > 2 && day >= 60)
-			{
-				day++;
-			}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-		}
-	}
-	else
-	{
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
+		}	
 		else
 		{
 			printf("Day of the year: %d\n", day);
