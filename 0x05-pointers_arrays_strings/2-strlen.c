7cc39f711dff4b3e951c82a4b69c65b61890e88d
David <4292@holbertonschool.com>
2022-02-14 15:02:27 +0000

    character count

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int_strlen - counts the amount of characters in a string
+ * @s: pointer, points to the value of a variable
+ * 
+ * Return: returns the amount of characters in string
+ */
+
+int _strlen(char *s)
+{
+	int charCount = 0;
+
+	while (s[charCount] != '\0')
+	{
+		charCount++;
+	}
+
+	return (charCount);
+}
