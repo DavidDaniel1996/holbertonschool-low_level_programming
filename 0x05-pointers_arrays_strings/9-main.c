cb40aafb1e82ff9517e46363018d14fe49a970a0
David <4292@holbertonschool.com>
2022-02-16 01:57:35 +0000

    copy fix

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -12,7 +12,7 @@ int main(void)
     char *ptr;
 
     ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
+    printf("%s", s1);
+    printf("%s", ptr);
     return (0);
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,7 +18,7 @@ char *_strcpy(char *dest, char *src)
 		length++;
 	}
 
-	length--;
+	/*length--;*/
 
 	for (count = 0; count < length && src[count] != '\0'; count++)
 	{
