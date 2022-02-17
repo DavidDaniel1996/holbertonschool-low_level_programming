8c2011258ce581a3aa9b896f19ee5ad191aa7f6b
David <4292@holbertonschool.com>
2022-02-17 17:40:15 +0000

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
+/**
+* _strcmp - compares two strings and determines which string is greater
+* @s1: string 1
+* @s2: string 2
+*
+* Return: 0 if equal, string one minus string two if non equal
+*/
+
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 < *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		return (-15);
+		s1++;
+		s2++;
 	}
-	if (*s1 > *s2)
-	{
-		return (15);
-	}
-	else 
+
+	if (s1 == s2)
 	{
 		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
 	}
 }
