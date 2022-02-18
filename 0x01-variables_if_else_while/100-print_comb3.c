549ca534bc7d5962c4ba1425cd7ea1c7a24cc2a7
David <4292@holbertonschool.com>
2022-02-18 15:44:07 +0000

    two digit combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+
+	for (x = '0' ; x <= '8' ; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (!(x == '0' && y == '0'))
+			{
+				putchar (x);
+				putchar (y);
+
+				if (!(x == '8' && y == '9'))
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
