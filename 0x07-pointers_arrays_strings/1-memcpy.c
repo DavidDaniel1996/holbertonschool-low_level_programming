8a95d29fcfad47336fcb5dec66e08e6209c32f19
David <4292@holbertonschool.com>
2022-02-22 15:06:36 +0000

    mem copy function

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0 ; count < n ; count ++)
+	{
+		dest[count] = src[count];
+	}
+	return(dest);
+}
