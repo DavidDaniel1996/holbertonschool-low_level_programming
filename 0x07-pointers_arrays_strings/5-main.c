c2613398e3e2076ee5e7354c5d21118377af0021
David <4292@holbertonschool.com>
2022-02-22 22:38:46 +0000

    checker test plus 2

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
+    char *s = "hello, world. this is a test.";
     char *f = "world";
     char *t;
 
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,13 +3,29 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int count;
+	int index;
 
-	for (count = 0; haystack[count] >= '\0'; count++)
+	for (count = 0; haystack[count] != '\0' ; count++)
 	{
-		if ((haystack + count) == needle)
+		for (index = 0; needle[index] != '\0'; index++)
+		{
+			if (haystack[count] == needle[index])
+			{
+				for (index = index; haystack[index + count] == needle[index]; index++)
+				{
+					;
+				}
+				return (haystack + ((count + index) + 2));
+			}
+			/*else 
+			{
+				break;
+			}
+	*/	}
+		/*if (s[count] == accept[index])
 		{
 			break;
-		}
+		}*/
 	}
-	return (haystack + count);
+	return ('\0');
 }
