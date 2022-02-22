5621f84da40eccf42f0a8e89cc40014b497b70c7
David <4292@holbertonschool.com>
2022-02-22 21:12:40 +0000

    checker test

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int count;
+
+	for (count = 0; haystack[count] >= '\0'; count++)
+	{
+		if ((haystack + count) == needle)
+		{
+			break;
+		}
+	}
+	return (haystack + count);
+}
