c46f52c804b8057a9ba3a153e40a2c149f72fa38
David <4292@holbertonschool.com>
2022-02-23 01:57:11 +0000

    betty compliant file

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of two diagonals
+ * @a: pointer
+ * @size: variable
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+
+}
