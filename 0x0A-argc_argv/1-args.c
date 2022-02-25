73772d9dbf7e80b6b61b3cc40c2279efe9d811cb
David <4292@holbertonschool.com>
2022-02-25 17:46:44 +0000

    betty fix

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - print number of arguments
+ * @argc: size of argv array
+ * @argv: array containing command line of arguments
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int count = 0;
