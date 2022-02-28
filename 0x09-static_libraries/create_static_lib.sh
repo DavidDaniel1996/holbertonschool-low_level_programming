5bd90714dc3c7efd58a218c7ec0b46ff43c85112
David <4292@holbertonschool.com>
2022-02-28 15:30:31 +0000

    code fix

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-ar cr liball.a 0-isupper.o 0-memset.o 0-strcat.o 100-atoi.o
-ar cr liball.a 1-isdigit.o 1-memcpy.o 1-strncat.o
-ar cr liball.a 2-strchr.o 2-strlen.o 2-strncpy.o
-ar cr liball.a 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o
-ar cr liball.a 4-isalpha.o 4-strpbrk.o
-ar cr liball.a 5-strstr.o 6-abs.o 9-strcpy.o _putchar.o
+gcc -c *.c
+ar cr liball.a *.o
