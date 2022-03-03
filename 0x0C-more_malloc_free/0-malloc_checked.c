c37d9a875cea85782ed313cb420ec594e94efdfc
David <4292@holbertonschool.com>
2022-03-03 17:27:24 +0000

    pointer to memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,17 @@
 #include <string.h>
 #include "main.h"
 
+/**
+* *malloc_checked - creates pointer that points to newly allocated memory
+* @b: amount of memory to be allocated)
+*
+* Return: ptr
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
+
 	if (ptr == NULL)
 	{
 		exit(98);
