eeebe2a7b7be597c916b3d1bd02d321e86ebc05c
David <4292@holbertonschool.com>
2022-03-03 17:24:48 +0000

    checker test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
