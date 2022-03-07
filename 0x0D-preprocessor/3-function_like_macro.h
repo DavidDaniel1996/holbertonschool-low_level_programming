d032a77afe6bf020e7f9b245086f128fb07fb93e
David <4292@holbertonschool.com>
2022-03-07 21:11:22 +0000

    betty fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,6 @@
-#ifndef ABS(x)
-#define ABS(x) ((x < 0) ? (-x) : (x))
-#endif /* ABS */
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x < 0) ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
