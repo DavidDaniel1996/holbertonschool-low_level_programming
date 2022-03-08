2d4c39d435af93c399d5d0c10a28124128c663ca
David <4292@holbertonschool.com>
2022-03-08 14:19:55 +0000

    checker test

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	/*if (d == NULL)
+	if (d != NULL)
 	{
-		d = malloc(sizeof(struct dog));
-	}*/
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
