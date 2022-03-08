2629dcb3a2e36dbab41549382e5a48e2d1046729
David <4292@holbertonschool.com>
2022-03-08 16:02:48 +0000

    test

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,6 +17,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 	d->name == NULL ?  printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-	d->age == '\0' ?  printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
+	/*d->age == '\0' ?  printf("Age: (nil)\n") : printf("Age: %f\n", d->age);*/
 	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
