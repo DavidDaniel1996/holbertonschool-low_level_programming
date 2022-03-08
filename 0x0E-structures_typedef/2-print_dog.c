786cde3ca095b55ce8d5082673420e06e8a31c09
David <4292@holbertonschool.com>
2022-03-08 17:14:08 +0000

    update dog.h

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,6 +18,5 @@ void print_dog(struct dog *d)
 	}
 	d->name == NULL ?  printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	/*d->age == '\0' ?  printf("Age: (nil)\n") : printf("Age: %f\n", d->age);*/
 	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,12 @@
  *
  */
 
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
