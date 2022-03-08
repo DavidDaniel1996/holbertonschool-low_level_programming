4902117c937fedbbd81da9a693931abd4de68238
David <4292@holbertonschool.com>
2022-03-08 20:14:15 +0000

    new doggy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,13 +4,13 @@
 #include "dog.h"
 
 /**
-* new_dog - creates new pupper
-* @name: name of dogoo
-* @age: age of pup
-* @owner: owner of good boi
-*
-* Return: void
-*/
+ * new_dog - creates new pupper
+ * @name: name of dogoo
+ * @age: age of pup
+ * @owner: owner of good boi
+ *
+ * Return: void
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -19,16 +19,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 	ptr->name = malloc((strlen(name) + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 	ptr->owner = malloc((strlen(owner) + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 
