aa9247176507b9a06bfbf11c28b6d911b1acfc30
David <4292@holbertonschool.com>
2022-03-08 14:15:15 +0000

    checker test

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	/*if (d == NULL)
+	{*/
+		d = malloc(sizeof(struct dog));
+	/*}*/
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
