035947f0178c5ed723df95e8e91f4097f3899a23
David <4292@holbertonschool.com>
2022-03-09 17:37:58 +0000

    betty fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 	{
 		return;
-	}	
+	}
 	for (count = 0; count < size; count++)
 	{
 		action(array[count]);
