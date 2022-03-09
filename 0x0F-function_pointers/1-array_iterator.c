0b6125ee4ef1d04acbf5bd8f6dd8ba37228d0c80
David <4292@holbertonschool.com>
2022-03-09 17:25:20 +0000

    execute function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes a function on elements of array
+* @array: pointer, points to array
+* @size: size of array
+* @action: pointer, points to function
+*
+* Return: void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int count;
+
+	for (count = 0; count < size; count++)
+	{
+		action(array[count]);
+	}
+}
