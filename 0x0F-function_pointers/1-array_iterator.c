76f0dd7a515f4cadbef4ef64ea6574a94490d18c
David <4292@holbertonschool.com>
2022-03-09 17:27:38 +0000

    edge case

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,18 +2,22 @@
 #include "function_pointers.h"
 
 /**
-* array_iterator - executes a function on elements of array
-* @array: pointer, points to array
-* @size: size of array
-* @action: pointer, points to function
-*
-* Return: void
-*/
+ * array_iterator - executes a function on elements of array
+ * @array: pointer, points to array
+ * @size: size of array
+ * @action: pointer, points to function
+ *
+ * Return: void
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int count;
 
+	if (array == NULL || size == NULL || action == NULL)
+	{
+		return;
+	}
 	for (count = 0; count < size; count++)
 	{
 		action(array[count]);
