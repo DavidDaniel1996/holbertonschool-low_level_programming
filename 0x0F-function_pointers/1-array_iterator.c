5826ef47abbe589ecd770da922d78788702754a4
David <4292@holbertonschool.com>
2022-03-09 17:37:02 +0000

    edge case

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int count;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}	
 	for (count = 0; count < size; count++)
 	{
 		action(array[count]);
