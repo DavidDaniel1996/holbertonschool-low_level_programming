5e177ed0d99da4aa46f0585bdc0e055df7f9a635
David <4292@holbertonschool.com>
2022-03-12 15:52:37 +0000

    print all function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints character
+ * @ptr: pointer to variadic argument
+ *
+ * Return: void
+ */
+
+void print_char(va_list ptr)
+{
+	char store;
+
+	store = va_arg(ptr, int);
+	printf("%c", store);
+}
+
+/**
+ * print_int - prints integer
+ * @ptr: pointer to variadic argument
+ *
+ * Return: void
+ */
+
+void print_int(va_list ptr)
+{
+	int store;
+
+	store = va_arg(ptr, int);
+	printf("%d", store);
+}
+
+/**
+ * print_float - prints float integer
+ * @ptr: pointer to variadic argument
+ *
+ * Return: void
+ */
+
+void print_float(va_list ptr)
+{
+	float store;
+
+	store = va_arg(ptr, double);
+	printf("%f", store);
+}
+
+/**
+ * print_string - prints string
+ * @ptr: pointer to variadic argument
+ *
+ * Return: void
+ */
+
+void print_string(va_list ptr)
+{
+	char *store;
+
+	store = va_arg(ptr, char *);
+	if (store == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", store);
+}
+
+/**
+ * print_all - variadic function thats prints any argument
+ * @format: string that specifies which data type to use
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ptr;
+	unsigned int count;
+	unsigned int idx;
+	char *sep;
+
+	type_t type[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+	};
+
+	va_start(ptr, format);
+
+	sep = "";
+	count = 0;
+	while (count < strlen(format))
+	{
+		idx = 0;
+		while (idx < 4)
+		{
+			if (*(format + count) == *(type[idx].data))
+			{
+				printf("%s", sep);
+				type[idx].f(ptr);
+				sep = ", ";
+			}
+			idx++;
+		}
+		count++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct type - structure containing data type and function pointer
+ * @data: variable to store data type
+ * @f: pointer to appropiate function
+ *
+ */
+
+typedef struct type
+{
+	char *data;
+	void (*f)(va_list ptr);
+} type_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
