56941f7bd9b91ed5357d5470d2d278b3dcb9da87
David <4292@holbertonschool.com>
2022-03-20 17:59:30 +0000

    possible output fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+	{
+		return;
+	}
+
 	while (*head != NULL)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
-	}
-	if (head == NULL)
-	{
-		return;
 	}
 }
