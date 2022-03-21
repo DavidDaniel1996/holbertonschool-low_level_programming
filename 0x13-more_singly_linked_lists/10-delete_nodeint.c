46fbfa9d94cfa5fc28b232121bd6411854fde210
David <4292@holbertonschool.com>
2022-03-21 15:59:03 +0000

    deletes nth node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index = deletes nth node
+ * @head: pointer to head node
+ * @index: node to be deleted
+ *
+ * Return: 1 if success, -1 if failure
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *head2;
+	unsigned int idx = 0;
+
+	head2 = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		tmp = *head;
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	while (idx < index - 1 && head2 != NULL)
+	{
+		tmp = head2->next;
+		head2 = tmp;
+		idx++;
+	}
+
+	if (head2 == NULL)
+	{
+		return (-1);
+	}
+	else
+	{
+		tmp = tmp->next;
+		head2->next = tmp->next;
+		free(tmp);
+		return (1);
+	}
+}
