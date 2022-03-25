f471fcfa72daf35d2ea8cb42a3de95f16b1ada54
David <4292@holbertonschool.com>
2022-03-25 19:02:47 +0000

    get value of bit at position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at given index
+ * @n: number
+ * @index: position of bit
+ *
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int value;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	value = (((n >> index) & 1));
+
+	return (value);
+}
