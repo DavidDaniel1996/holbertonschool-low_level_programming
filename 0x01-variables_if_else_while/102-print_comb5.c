912010b04c875bcb918328768dc7ae6c86fc42a7
David <4292@holbertonschool.com>
2022-03-26 16:12:34 +0000

    Print combo of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints possible two digit combinations
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int a, b, x, y;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 8; b++)
+		{
+			for (x = 0; x <= 9; x++)
+			{
+				for (y = 0; y <= 9; y++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					while (a == x && y <= b)
+					{
+						y++;
+					}
+					putchar(x + '0');
+					putchar(y + '0');
+					if (!((a == 9 && b == 8) && (x == 9 && y == 9)))
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
