5ada5d29bc24d415decece065bac9856fe31f9cc
David <4292@holbertonschool.com>
2022-03-29 19:25:23 +0000

    fix output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fo_from = open(argv[1], O_RDONLY);
-	fo_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fo_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	while (fr > 0)
 	{
