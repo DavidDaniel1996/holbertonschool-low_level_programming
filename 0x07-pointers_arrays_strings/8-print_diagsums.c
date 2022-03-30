ce9bbc904f9ca66de390198b15fad57c570730c4
David <4292@holbertonschool.com>
2022-03-30 17:43:03 +0000

    adds elements in diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,5 +11,22 @@
 
 void print_diagsums(int *a, int size)
 {
+	int idx;
+	int store1 = 0, store2 = 0;
 
+	for (idx = 0; idx < size; idx++)
+	{
+		store1 += a[idx];
+		a = a + size;
+	}
+	printf("%d, ", store1);
+
+	a = a - size;
+
+	for (idx = 0; idx < size; idx++)
+	{
+		store2 += a[idx];
+		a = a - size;
+	}
+	printf("%d\n", store2);
 }
