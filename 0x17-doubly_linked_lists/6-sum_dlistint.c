a8af2b38cbc4a9c3547b5cee0c908449e2c074f2
David <4292@holbertonschool.com>
2022-05-05 15:59:39 +0000

    Betty fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,20 +9,20 @@
 
 int sum_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
-    int sum = 0;
+	dlistint_t *tmp;
+	int sum = 0;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
+	if (head == NULL)
+	{
+		return (0);
+	}
 
-    while (head != NULL)
-    {
-        sum += head->n;
-        tmp = head->next;
-        head = tmp;
-    }
+	while (head != NULL)
+	{
+		sum += head->n;
+		tmp = head->next;
+		head = tmp;
+	}
 
-    return (sum);
+	return (sum);
 }
