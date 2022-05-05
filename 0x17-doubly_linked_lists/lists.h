a61bbeb5f7157932f545d0e8107285568dea4f34
David <4292@holbertonschool.com>
2022-05-05 11:13:07 -0400

    added library

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,6 +18,7 @@ struct dlistint_s *next;
 } dlistint_t;
 
 #include <stdio.h>
+#include <stdlib.h>
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
