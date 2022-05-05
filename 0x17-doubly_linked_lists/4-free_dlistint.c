0a177792bec7b07ca228a50b29ca30045e884d30
David <4292@holbertonschool.com>
2022-05-05 15:40:38 +0000

    Betty fix

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,18 +3,18 @@
 /**
  * free_dlistint - frees list
  * @head: pointer to head node
- * 
- * Return: void 
+ *
+ * Return: void
  */
 
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
+	dlistint_t *tmp;
 
-    while (head != NULL)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
-    }
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
