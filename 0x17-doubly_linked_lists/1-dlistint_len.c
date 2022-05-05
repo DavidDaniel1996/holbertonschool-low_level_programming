3f6b9519bb805f107f7846cdb01d571a2a027454
David <4292@holbertonschool.com>
2022-05-05 09:53:57 -0400

    count number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of elements
+ * @h: pointer to node
+ * 
+ * Return: number of elements 
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    int count;
+
+    if (h == NULL)
+    {
+        return ('\0');
+    }
+
+    for (count = 0; h != NULL; count++)
+    {
+        h = h->next;
+    }
+
+    return (count);
+}
