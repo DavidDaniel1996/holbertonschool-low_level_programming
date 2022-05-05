165f5836f18b914ec9b7288d0550f0325bac0ed0
David <4292@holbertonschool.com>
2022-05-05 11:58:55 -0400

    sum of integers in list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns sum of all intengers in list
+ * @head: pointer to head node
+ *
+ * Return: sum of all intengers
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *tmp;
+    int sum = 0;
+
+    if (head == NULL)
+    {
+        return (0);
+    }
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        tmp = head->next;
+        head = tmp;
+    }
+
+    return (sum);
+}
