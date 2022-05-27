59706c4594738094c52e9d5a8e2faeaa4eb385fe
David <4292@holbertonschool.com>
2022-05-27 12:19:03 -0400

    add new element to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: key of the element
+ * @value: value of the element
+ *
+ * Return: 1 if succesful, 0 otherwise
+ */
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+
+	index = key_index(key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	printf("%ld\n", index);
+	return (1);
+}
