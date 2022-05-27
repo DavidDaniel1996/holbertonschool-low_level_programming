0bc125669b3b5aa87e4c3828fa87ab4d160c1e3c
David <4292@holbertonschool.com>
2022-05-27 10:38:41 -0400

    Handled edge cases

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,8 +13,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int  i;
 
 	new_hash = malloc(sizeof(hash_table_t));
+	if (new_hash == NULL)
+	{
+		return (NULL);
+	}
 	new_hash->size = size;
 	new_hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash->array == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		new_hash->array[i] = NULL;
