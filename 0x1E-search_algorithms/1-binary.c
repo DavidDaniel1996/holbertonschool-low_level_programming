3c372554696c1130d013bc1dd2ae4ae5a211ea85
David <4292@holbertonschool.com>
2022-08-30 15:23:43 -0400

    no change

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,40 +11,40 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t idx;
-	size_t min = 0;
-	size_t max = size - 1;
-	size_t middle;
+    size_t idx;
+    size_t min = 0;
+    size_t max = size - 1;
+    size_t middle;
 
-	if (array == NULL)
-	{
-		return (-1);
-	}
+    if (array == NULL)
+    {
+        return (-1);
+    }
 
-	while (min <= max)
-	{
-		printf("Searching in array: ");
-		for (idx = min; idx <= max; idx++)
-		{
-			if (idx == max)
-				printf("%i\n", array[idx]);
-			else
-				printf("%i, ", array[idx]);
-		}
+    while (min <= max)
+    {
+        printf("Searching in array: ");
+        for (idx = min; idx <= max; idx++)
+        {
+            if (idx == max)
+                printf("%i\n", array[idx]);
+            else
+                printf("%i, ", array[idx]);
+        }
 
-		middle = (min + max) / 2;
-		if (value == array[middle])
-		{
-			return (middle);
-		}
-		else if (value < array[middle])
-		{
-			max = array[middle - 1];
-		}
-		else if (value > array[middle])
-		{
-			min = array[middle + 1];
-		}
-	}
-	return (-1);
+        middle = (min + max) / 2;
+        if (value == array[middle])
+        {
+            return (middle);
+        }
+        else if (value < array[middle])
+        {
+            max = array[middle - 1];
+        }
+        else if (value > array[middle])
+        {
+            min = array[middle + 1];
+        }
+    }
+    return (-1);
 }
